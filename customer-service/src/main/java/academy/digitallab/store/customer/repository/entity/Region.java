56a9e0c116db337074d9025dd0beb1e7c66769cd
cesar munoz <cesar.munoz@gac-sa.cl>
2024-02-08 13:20:00 -0300

    nada

--- customer-service/src/main/java/academy/digitallab/store/customer/repository/entity/Region.java
+++ customer-service/src/main/java/academy/digitallab/store/customer/repository/entity/Region.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package academy.digitallab.store.customer.repository.entity;
 
 import lombok.Data;
@@ -19,25 +18,3 @@ public class Region implements Serializable {
 
 
 }
-=======
-package academy.digitallab.store.customer.repository.entity;
-
-import lombok.Data;
-
-import javax.persistence.*;
-import java.io.Serializable;
-
-@Data
-@Entity
-@Table(name = "tbl_regions")
-public class Region implements Serializable {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-
-
-
-}
->>>>>>> 57ff53b9224c304ee6f2bf3174dd8eea5fe5ed64
